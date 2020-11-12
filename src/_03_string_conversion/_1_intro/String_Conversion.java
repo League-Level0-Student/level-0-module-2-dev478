f1883a448227270454daa48ea8e8322c48292267
root <root@a9c989da7331>
2020-11-12 00:57:08 +0000

    Roller coaster in progress, string conversions and string cannot add done

---
+++ src/_03_string_conversion/_1_intro/String_Conversion.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package _03_string_conversion._1_intro;
+
+import javax.swing.JOptionPane;
+
+/**
+ * @author root
+ *
+ */
+public class String_Conversion {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String value = JOptionPane.showInputDialog("Enter a number.");
+		int number = Integer.parseInt(value);
+	}
+
+}
--- src/_03_string_conversion/_3_rollercoaster/RollerCoaster.java
+++ src/_03_string_conversion/_3_rollercoaster/RollerCoaster.java
@@ -1,18 +1,22 @@
 package _03_string_conversion._3_rollercoaster;
 
+import java.nio.charset.MalformedInputException;
+
 import javax.swing.JOptionPane;
 
 public class RollerCoaster {
 
 	// 1) Make a main method that includes all the steps below…. 
+		public static void main(String[] args) {
+			
 		
 		// 2) Ask the user to enter their height in inches using JOptionPane and set it to the variable heightToRide
-		
+		String heightToRide = JOptionPane.showInputDialog("Enter your height, in inches.");
 		// Uncomment the line below...
-			//if( heightToRide > 42 ) { }
+			if( heightToRide > 42 ) { }
 		
 		// Do you see the heightToRide > 42 underlined red? That is because you are comparing a STRING to an INTEGER.
-		
+		//yes is see
 		// 3) Convert heightToRide to heightAsInt using Integer.parseInt();  Hint: int heightAsInt = Integer.parse... 
 	
 		// 4) If the heightAsInt is greater than or equal to 48 inches tall print:
@@ -23,3 +27,4 @@ public class RollerCoaster {
 	
 		// 6) else print: "You must be at least 42 inches tall to ride the roller coaster pal!"
 } 
+}
